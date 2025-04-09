ff7823a95ba3a61c82cad0d911bf5c96b4722f9d
keebs5225 <132247636+keebs5225@users.noreply.github.com>
2025-04-09 08:16:57 -0500

    Deploy static frontend to gh-pages

--- src/services/prismaService.js
+++
@@ -1,6 +0,0 @@
-//prismaService.js
-
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();  // Initialize Prisma client
-
-module.exports = prisma;  // Export Prisma client for use in controllers
